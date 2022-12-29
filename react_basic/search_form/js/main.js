2808f4ed16e7a107b606abe8792a1fca65cec7b9
seohyun <jenkang603@gmail.com>
2022-12-29 12:48:47 +0900

    Start

---
+++ react_basic/search_form/js/main.js
@@ -0,0 +1,19 @@
+class App extends React.Component {
+    render() {
+
+        return (
+            <>
+
+                <header className="container">
+                    <h2>검색</h2>
+                </header>
+
+                <div className="container">
+
+                </div>
+            </>
+        );
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#app'));
