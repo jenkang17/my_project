2a0f6c798ea25ec18824244e1e345839d3de00be
seohyun <jenkang603@gmail.com>
2023-01-09 16:44:42 +0900

    tamplate 7

--- USE/GSAP/Pinning/js/script.js
+++ USE/GSAP/Pinning/js/script.js
@@ -1,12 +1,32 @@
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
+
 gsap.registerPlugin(ScrollTrigger);
-gsap.defaults({ease:"power1", duration:1});
+gsap.defaults({ease:"power1", duration:5});
 
 const tl = gsap.timeline();
 const sections = document.querySelectorAll('.section');
 
-tl.from(".section2", {xPercent:100})
-    .from(".section3", {xPercent:-100})
-    .from(".section4", {yPercent:-100})
+
+if (isMobile)
+{
+    tl.from(".section2", {xPercent:100})
+    .from(".section3", {xPercent:100})
+    .from(".section4", {xPercent:100})
+    .from(".section5", {xPercent:100})
+    .from(".section6", {xPercent:100})
+    .from(".section7", {xPercent:100})
+
+}
+else 
+{
+    tl.from(".section2", {xPercent:100})
+    .from(".section3", {xPercent:100})
+    .from(".section4", {yPercent:100})
+    .from(".section5", {xPercent:100})
+    .from(".section6", {xPercent:-100})
+    .from(".section7", {yPercent:-100})
+}
+
 
 ScrollTrigger.create({
     animation: tl,
@@ -19,8 +39,15 @@ ScrollTrigger.create({
 })
 
 function gotoslide(i) {
+
     const section = document.getElementsByClassName("section");
     const target = document.querySelector(".section" + i);
+
+    if (isMobile)
+    {
+        $(".navbar-toggler").addClass("collapsed");
+        $(".navbar-collapse").removeClass("show");
+    }
 
     for (let j = 0; j < section.length; j++)
     {
@@ -39,3 +66,24 @@ document.addEventListener('scroll', function(){
     }
 })
 
+function boardView(n){
+    $("#boardView").modal('show');
+}
+
+function boardWrite() {
+    $("#boardWrite").modal('show');
+}
+
+function login(){
+    $("#login").modal('show');
+}
+
+function register(){
+    $("#register").modal('show');
+}
+
+
+
+//$(".game-box").hover(function(){
+    //gsap.to($(this).find(".game-img"), { duration: 0.3, ease: "slow(0.7, 0.7, false)", y: -10 });
+//})
